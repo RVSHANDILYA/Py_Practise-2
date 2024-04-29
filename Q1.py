83bdf55c300518a4fe4ed4325238879cc08c68c8
RVSHANDILYA <shandilyapuram544@gmail.com>
2024-04-29 16:17:25 +0530

    first number is divisible by the second ?

---
+++ Q1.py
@@ -0,0 +1,11 @@
+'''
+Q1.Write a program that takes two numbers as input and checks if the first
+number is divisible by the second.
+'''
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+
+if a%b==0:
+    print(f"The number {a} is divisible by {b}")
+else:
+    print("The number is not visible !!")
